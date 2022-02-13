d307cc5d572dcb5bbc547beb192dbe8a688fe22f
Kerian <kerian.allaire@gmail.com>
2022-02-13 17:56:21 +0100

    CA MARCHE POUR CAT

--- src/parsing/ast.c
+++ src/parsing/ast.c
@@ -8,16 +8,20 @@
 
 ast_node* creating_ast(listT* list)
 {
-    ast_node first = {
-        NODE_HEAD,
-        .data.node_head = NULL,
-        "tree",
-        0,
-        NULL,
-        NULL,
-        NULL,
-    };
-    ast_node* current = &first;
+    ast_node* current = calloc(sizeof(ast_node),1);
+    current->type = NODE_HEAD;
+    current->data.node_head = NULL;
+    char* str = malloc(sizeof(char)*5);
+    str[0]='t'; 
+    str[1]='r';
+    str[2]='e';
+    str[3]='e';
+    str[4]='\0';
+    current->string = str;
+    current->nb_child=0;
+    current->father = NULL;
+    current->child = NULL;
+    current->sibling = NULL;
     list_elm* p = list->head;
     int is_backtick = 0;
     size_t nb_parenthesis = 0;
@@ -60,16 +64,14 @@ ast_node* creating_ast(listT* list)
             current->data.node_left_paren->closed =1;
             p=p->next;
         }
-        ast_node new = {
-        NODE_HEAD,
-        .data.node_head = NULL,
-        t.string,
-        0,
-        NULL,
-        NULL,
-        NULL,
-        };
-
+        ast_node* new = calloc(sizeof(ast_node),1);
+        new->type = NODE_HEAD;
+        new->data.node_head = NULL;
+        new->string = t.string;
+        new->child = 0;
+        new->father = NULL;
+        new->child = NULL;
+        new->sibling = NULL;
         switch(t.type)
         {
             case COMMAND:
@@ -91,18 +93,18 @@ ast_node* creating_ast(listT* list)
                 {
                     //erreur de grammaire
                 }
-                new.type = NODE_COMMAND;
-                create_command(&new);
-                new.father = current;
+                new->type = NODE_COMMAND;
+                create_command(new);
+                new->father = current;
                 current->nb_child++;
                 if(current->child ==NULL)
-                    current->child = &new;
+                    current->child = new;
                 else
-                    current->child->sibling=&new;
+                    current->child->sibling=new;
                 break;
             case ARGUMENT:
-                new.type = NODE_ARGUMENT;
-                new.father = current;
+                new->type = NODE_ARGUMENT;
+                new->father = current;
                 //check si le current est un argument, ou si le père est un 
                 //séparateur ou une commande et que son premier fils est diffé
                 //de NULL
@@ -111,8 +113,8 @@ ast_node* creating_ast(listT* list)
                      is_backtick == 0)) &&
                    current->father->type==NODE_COMMAND)
                 {
-                    current->sibling = &new;
-                    new.father = current->father;
+                    current->sibling = new;
+                    new->father = current->father;
                     current->father->nb_child++;
                 }
                 else if(current->type==NODE_ARGUMENT ||
@@ -129,16 +131,16 @@ ast_node* creating_ast(listT* list)
                 else if(is_chevron(current))
                 {
                     current = current->child;
-                    current->sibling = &new;
-                    new.father = current->father;
+                    current->sibling = new;
+                    new->father = current->father;
                     current->father->nb_child++;
                 }
                 else
                 {
-                    current->child = &new;
-                    new.father = current;
+                    current->child = new;
+                    new->father = current;
                 }
-                create_argument(&new);
+                create_argument(new);
                 break;
             case AND_BOOL:
                 if(is_separator(current))
@@ -155,12 +157,12 @@ ast_node* creating_ast(listT* list)
                     //car le && doit avoir lieu après l'exec des précédents
                     current = current->father;
                 }
-                new.type = NODE_AND_BOOL;
-                create_and_bool(&new);
-                new.father = current->father;
-                current->father = &new;
-                new.child = current;
-                new.nb_child ++;
+                new->type = NODE_AND_BOOL;
+                create_and_bool(new);
+                new->father = current->father;
+                current->father = new;
+                new->child = current;
+                new->nb_child ++;
                 break;
             case OR_BOOL:
                 if(is_separator(current))
@@ -177,12 +179,12 @@ ast_node* creating_ast(listT* list)
                     //car le || doit avoir lieu après l'exec des précédents
                     current = current->father;
                 }
-                new.type = NODE_OR_BOOL;
-                create_or_bool(&new);
-                new.father = current->father;
-                current->father = &new;
-                new.child = current;
-                new.nb_child ++;
+                new->type = NODE_OR_BOOL;
+                create_or_bool(new);
+                new->father = current->father;
+                current->father = new;
+                new->child = current;
+                new->nb_child ++;
                 break;
             case AND:
                 if(is_separator(current))
@@ -199,12 +201,12 @@ ast_node* creating_ast(listT* list)
                     //car le & doit avoir lieu après l'exec des précédents
                     current = current->father;
                 }
-                new.type = NODE_AND;
-                create_and(&new);
-                new.father = current->father;
-                current->father=&new;
-                new.child = current;
-                new.nb_child++;
+                new->type = NODE_AND;
+                create_and(new);
+                new->father = current->father;
+                current->father=new;
+                new->child = current;
+                new->nb_child++;
                 break;
             case DRIGHT_CHEVRON:
                 if(is_separator(current))
@@ -221,12 +223,12 @@ ast_node* creating_ast(listT* list)
                     //car le >> doit avoir lieu après l'exec des précédents
                     current = current->father;
                 }
-                new.type = NODE_DRIGHT_CHEVRON;
-                create_dright_chevron(&new);
-                new.father = current->father;
-                current->father=&new;
-                new.child = current;
-                new.nb_child++;
+                new->type = NODE_DRIGHT_CHEVRON;
+                create_dright_chevron(new);
+                new->father = current->father;
+                current->father=new;
+                new->child = current;
+                new->nb_child++;
                 break;
             case DLEFT_CHEVRON:
                 if(is_separator(current))
@@ -243,12 +245,12 @@ ast_node* creating_ast(listT* list)
                     //car le << doit avoir lieu après l'exec des précédents
                     current = current->father;
                 }
-                new.type = NODE_DLEFT_CHEVRON;
-                create_dleft_chevron(&new);
-                new.father = current->father;
-                current->father=&new;
-                new.child = current;
-                new.nb_child++;
+                new->type = NODE_DLEFT_CHEVRON;
+                create_dleft_chevron(new);
+                new->father = current->father;
+                current->father=new;
+                new->child = current;
+                new->nb_child++;
                 break;
             case RIGHT_CHEVRON:
                 if(is_separator(current))
@@ -260,12 +262,12 @@ ast_node* creating_ast(listT* list)
                     //car le > doit avoir lieu après l'exec des précédents
                     current = current->father;
                 }
-                new.type = NODE_RIGHT_CHEVRON;
-                create_right_chevron(&new);
-                new.father = current->father;
-                current->father=&new;
-                new.child = current;
-                new.nb_child++;
+                new->type = NODE_RIGHT_CHEVRON;
+                create_right_chevron(new);
+                new->father = current->father;
+                current->father=new;
+                new->child = current;
+                new->nb_child++;
                 break;
             case LEFT_CHEVRON:
                 if(is_separator(current))
@@ -282,12 +284,12 @@ ast_node* creating_ast(listT* list)
                     //car le < doit avoir lieu après l'exec des précédents
                     current = current->father;
                 }
-                new.type = NODE_LEFT_CHEVRON;
-                create_left_chevron(&new);
-                new.father = current->father;
-                current->father=&new;
-                new.child = current;
-                new.nb_child++;
+                new->type = NODE_LEFT_CHEVRON;
+                create_left_chevron(new);
+                new->father = current->father;
+                current->father=new;
+                new->child = current;
+                new->nb_child++;
                 break;
             case SEMI_COLON:
                 if(is_separator(current))
@@ -304,12 +306,12 @@ ast_node* creating_ast(listT* list)
                     //car le semi_colon doit avoir lieu après l'exec des précédents
                     current = current->father;
                 }
-                new.type = NODE_SEMI_COLON;
-                create_semi_colon(&new);
-                new.father = current->father;
-                current->father=&new;
-                new.child = current;
-                new.nb_child++;
+                new->type = NODE_SEMI_COLON;
+                create_semi_colon(new);
+                new->father = current->father;
+                current->father=new;
+                new->child = current;
+                new->nb_child++;
                 break;
             case LEFT_PAREN:
                 while(!is_separator(current) &&
@@ -329,14 +331,14 @@ ast_node* creating_ast(listT* list)
                 {
                     //erreur de grammaire
                 }
-                new.type = NODE_LEFT_PAREN;
-                create_left_paren(&new);
-                new.father = current;
+                new->type = NODE_LEFT_PAREN;
+                create_left_paren(new);
+                new->father = current;
                 current->nb_child++;
                 if(current->child ==NULL)
-                    current->child = &new;
+                    current->child = new;
                 else
-                    current->child->sibling=&new;
+                    current->child->sibling=new;
                 break;
             case PIPE:
                 if(is_separator(current))
@@ -353,22 +355,22 @@ ast_node* creating_ast(listT* list)
                     //car le pipe doit avoir lieu après l'exec des précédents
                     current = current->father;
                 }
-                new.type = NODE_PIPE;
-                create_pipe(&new);
-                new.father = current->father;
-                current->father=&new;
-                new.child = current;
-                new.nb_child++;
+                new->type = NODE_PIPE;
+                create_pipe(new);
+                new->father = current->father;
+                current->father=new;
+                new->child = current;
+                new->nb_child++;
                 break;
             case BACKTICK:
                 is_backtick = 1;
-                new.type = NODE_BACKTICK;
+                new->type = NODE_BACKTICK;
                 if((current->type==NODE_ARGUMENT ||
                     current->type == NODE_BACKTICK) &&
                     current->father->type==NODE_COMMAND)
                 {
-                    current->sibling = &new;
-                    new.father = current->father;
+                    current->sibling = new;
+                    new->father = current->father;
                     current->father->nb_child++;
                 }
                 else if(current->type==NODE_ARGUMENT ||
@@ -384,22 +386,22 @@ ast_node* creating_ast(listT* list)
                 else if(is_chevron(current))
                 {
                     current = current->child;
-                    current->sibling = &new;
-                    new.father = current->father;
+                    current->sibling = new;
+                    new->father = current->father;
                     current->father->nb_child++;
                 }
                 else
                 {
-                    current->child = &new;
-                    new.father = current;
+                    current->child = new;
+                    new->father = current;
                 }
-                create_backtick(&new);
+                create_backtick(new);
                 break;
             default:
                 //unidentified token
                 break;
         }
-        current = &new;
+        current = new;
         p = p->next;
     }
     if(nb_parenthesis != 0)
@@ -477,8 +479,10 @@ void free_ast(ast_node* node)
             //an error occured
             break;
     }
-    free_ast(node->child);
-    free_ast(node->sibling);
+    if(node->child != NULL)
+        free_ast(node->child);
+    if(node->sibling != NULL)    
+        free_ast(node->sibling);
     free(node);
 }
 
@@ -521,114 +525,111 @@ int is_chevron(ast_node* current)
 
 void create_or_bool(ast_node* new)
 {
-    struct node_or_bool new_data = {
-      new,  
-    };
-    new->data.node_or_bool=&new_data;
+    struct node_or_bool* new_data = calloc(sizeof(struct node_or_bool),1);
+    new_data->node = new;
+    new->data.node_or_bool=new_data;
 }
 
 void create_left_paren(ast_node* new)
 {
-    struct node_left_paren new_data = {
-      new,
-      0,
-    };
-    new->data.node_left_paren=&new_data;
+    struct node_left_paren* new_data = calloc(sizeof(struct node_left_paren),1);
+    new_data->node = new;
+    new_data->closed = 0;
+    new->data.node_left_paren=new_data;
 }
 
 //<<
 void create_dleft_chevron(ast_node* new)
 {
-    struct node_dleft_chevron new_data ={
-      new,  
-    };
-    new->data.node_dleft_chevron=&new_data;
+    struct node_dleft_chevron* new_data =
+        calloc(sizeof(struct node_dleft_chevron),1);
+    new_data->node=new;
+    new->data.node_dleft_chevron=new_data;
 }
 
 //>>
 void create_dright_chevron(ast_node* new)
 {
-    struct node_dright_chevron new_data = {
-      new,  
-    };
-    new->data.node_dright_chevron=&new_data;
+    struct node_dright_chevron* new_data=
+        calloc(sizeof(struct node_dright_chevron),1); 
+    new_data->node = new;
+    new->data.node_dright_chevron=new_data;
 }
 
 //<
 void create_left_chevron(ast_node* new)
 {
-    struct node_left_chevron new_data = 
-        {
-      new,  
-    };
-    new->data.node_left_chevron=&new_data;
+    struct node_left_chevron* new_data = 
+        calloc(sizeof(struct node_left_chevron),1);
+    new_data->node = new;
+    new->data.node_left_chevron=new_data;
 }
 
 //>
 void create_right_chevron(ast_node* new)
 {
-    struct node_right_chevron new_data = {
-      new,  
-    };
-    new->data.node_right_chevron=&new_data;
+    struct node_right_chevron* new_data = 
+        calloc(sizeof(struct node_right_chevron),1);
+    new_data->node = new;
+    new->data.node_right_chevron=new_data;
 }
 
 void create_and_bool(ast_node* new)
 {
-    struct node_and_bool new_data ={
-      new,  
-    };
-    new->data.node_and_bool = &new_data;
+    struct node_and_bool* new_data = 
+        calloc(sizeof(struct node_and_bool),1);
+    new_data->node = new;
+    new->data.node_and_bool = new_data;
 }
 
 void create_argument(ast_node *new)
 {
-    struct node_argument new_data ={
-      new,  
-    };
-    new->data.node_argument = &new_data;
+    struct node_argument* new_data = 
+        calloc(sizeof(struct node_argument),1);
+    new_data->node = new;
+    new->data.node_argument = new_data;
 }
 
 void create_command(ast_node *new)
 {
-    struct node_command new_data = {
-      DEFAULT,
-      new,
-    };
-    new->data.node_command = &new_data;
+    struct node_command* new_data = 
+        calloc(sizeof(struct node_command),1);
+    new_data->node = new;
+    new_data->cmd = DEFAULT;
+    new->data.node_command = new_data;
     check_command(new);
 }
 
 void create_and(ast_node* new)
 {
-    struct node_and new_data = {
-      new,  
-    };
-    new->data.node_and=&new_data;
+    struct node_and* new_data = 
+        calloc(sizeof(struct node_and),1);
+    new_data->node = new;
+    new->data.node_and=new_data;
 }
 
 void create_semi_colon(ast_node* new)
 {
-    struct node_semi_colon new_data = {
-      new,  
-    };
-    new->data.node_semi_colon=&new_data;
+    struct node_semi_colon* new_data =
+        calloc(sizeof(struct node_semi_colon),1);
+    new_data->node = new;
+    new->data.node_semi_colon=new_data;
 }
 
 void create_pipe(ast_node* new)
 {
-    struct node_pipe new_data = {
-      new,  
-    };
-    new->data.node_pipe=&new_data;
+    struct node_pipe* new_data = 
+        calloc(sizeof(struct node_pipe),1);
+    new_data->node = new;
+    new->data.node_pipe=new_data;
 }
 
 void create_backtick(ast_node* new)
 {
-    struct node_backtick new_data = {
-      new,  
-    };
-    new->data.node_backtick=&new_data;
+    struct node_backtick* new_data = 
+        calloc(sizeof(struct node_backtick),1);
+    new_data->node = new;
+    new->data.node_backtick=new_data;
 }
 
 //function to check errors for command
--- src/parsing/main.c
+++ src/parsing/main.c
@@ -7,7 +7,7 @@
 
 int __ast_print(ast_node* ast,int i)
 {
-    printf("|\n");
+    printf("|");
     for(int j=0;j<i;j++)
     {
 	printf("_");
@@ -37,23 +37,27 @@ int ast_print(ast_node* ast)
 
 int main()
 {
-    struct listT l;
-    list_initT(&l);
+    struct listT* l = malloc(sizeof(listT));
+    list_initT(l);
 
     //init tokens
-    token c1 = {
-        COMMAND,
-        "cat",
-        5
-    };
+    token* c1 = malloc(sizeof(token));
+    char* str = malloc(sizeof(char)*4);
+    str[0]='c';
+    str[1]='a';
+    str[2]='t';
+    str[3]='\0';
+    c1->type=COMMAND;
+    c1->string = str;
+    c1->len = 3;
 
-    list_push_endT(&l, &c1); //cat
 
-    ast_node* node= creating_ast(&l);
+    list_push_endT(l, c1); //cat
 
+    ast_node* node= creating_ast(l);
     int err = ast_print(node);
     
     free_ast(node);
-    freeL(&l);
+    freeL(l);
     return err;
 }
